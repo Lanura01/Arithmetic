f2de3a68a25e01c7e0f59b1ab8884d94995f4d85
DeA Shannon <Deasjia.shannon@gmail.com>
2023-09-13 11:10:19 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1;
+        int num2;
+        Console.Write("Input the first number: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Input the second number");
+        num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"The total is: {sum}");    }
+}
